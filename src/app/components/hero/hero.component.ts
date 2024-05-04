fc8d266d7af5d2d080279f03e06a2be31ab263d5
Gustavo Larsen <guslarsen@gmail.com>
2024-05-03 22:47:55 -0300

    feat: adiciona sistema de mensagem na aplicacao ao selecionar um Heroe na lsita

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,6 +2,7 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { HeroService } from '../../services/hero.service';
+import { MessageService } from '../../services/message.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
@@ -14,7 +15,10 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 export class HeroComponent {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   getHeros(): void {
     this.heroService.getHeroes().subscribe((data) => (this.heroes = data));
@@ -28,5 +32,6 @@ export class HeroComponent {
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroComponent: Selected hero id=${hero.id}`);
   }
 }
